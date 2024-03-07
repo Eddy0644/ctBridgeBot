88f6f6b928f48ebb180737930ddc65785ecc2f58
Eddy0644 <30020507+Eddy0644@users.noreply.github.com>
2024-03-07 21:52:31 +0800

    update project structure #1

--- config/confLoader.js
+++ config/confLoader.js
@@ -1,7 +1,8 @@
 // confLoader.js
 
 const defaultConfig = require("./def.conf.js");
-const userConfigPath = require("path").join(__dirname, "user.conf.js");
+// const userConfigPath = require("path").join(__dirname, "user.conf.js");
+const userConfigPath = "../data/user.conf.js";
 const {ctLogger} = require('../src/common')("lite");
 
 function mergeConfig(defaultConfig, userConfig) {
