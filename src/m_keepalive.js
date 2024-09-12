938ca6df1f9c166b6f52a4c0e14d7ceeac6301ec
Eddy0644 <30020507+Eddy0644@users.noreply.github.com>
2024-09-12 11:10:18 +0800

    fix. keepalive module errors

--- src/m_keepalive.js
+++ src/m_keepalive.js
@@ -2,31 +2,31 @@ const dayjs = require("dayjs");
 let env;
 
 async function triggerCheck() {
-    const {secret, state} = env;
+    const {secret, state, wxLogger} = env;
     const t_conf = secret.mods.keepalive;
-    const t_stv = state.v.keepalive;
+    const t_state = state.v.keepalive;
     // Parse trigger timespan
     const {start, end} = t_conf.trigger_v1[0];
     const [startHour, startMinute] = start.split(':').map(Number), [endHour, endMinute] = end.split(':').map(Number);
     const startTime = dayjs().startOf('minute').hour(startHour).minute(startMinute);
     const endTime = dayjs().startOf('minute').hour(endHour).minute(endMinute);
-    const isNowBetween = dayjs().isAfter(startTime) && now.isBefore(endTime);
+    const isNowBetween = dayjs().isAfter(startTime) && dayjs().isBefore(endTime);
 
     // If within range, start checking
     if (isNowBetween) {
-        if (secret.misc.debug_show_additional_log) console.log("Report on keepalive.triggerCheck #1: ", t_stv.msgCounter_prev, " ", state.v.wxStat.MsgTotal);
+        if (secret.misc.debug_show_additional_log) wxLogger.debug("Report on keepalive.triggerCheck #1: ", t_state.msgCounter_prev, " ", state.v.wxStat.MsgTotal);
 
-        if (t_stv.msgCounter_prev < state.v.wxStat.MsgTotal) {
+        if (t_state.msgCounter_prev < state.v.wxStat.MsgTotal) {
             // There are new messages since last timer run, so update idle timer
-            t_stv.msgCounter_prev = state.v.wxStat.MsgTotal;
-            t_stv.idle_start_ts = dayjs().unix();
+            t_state.msgCounter_prev = state.v.wxStat.MsgTotal;
+            t_state.idle_start_ts = dayjs().unix();
         } else {
             // No new messages since last timer run, so check if idle timer exceeds
-            const idle_length = dayjs().unix() - t_stv.idle_start_ts;
+            const idle_length = dayjs().unix() - t_state.idle_start_ts;
             const idle_max = t_conf.trigger_v1[0].max_idle_minutes * 60;
-            if (t_stv.idle_start_ts !== 0 && idle_length > idle_max) {
+            if (t_state.idle_start_ts !== 0 && idle_length > idle_max) {
                 // Idle timer exceeds, so trigger keepalive
-                ctLogger.info(`Keepalive triggered: last update of idle timer is ${dayjs(t_stv.idle_start_ts).format("HH:mm")}, which exceeds ${t_conf.trigger_v1[0].max_idle_minutes} minutes from now.`);
+                wxLogger.info(`Keepalive triggered: last update of idle timer is ${dayjs(t_state.idle_start_ts).format("HH:mm")}, which exceeds ${t_conf.trigger_v1[0].max_idle_minutes} minutes from now.`);
                 // Check functions should be executed here...
                 // First perform avatar-url check
                 if (t_conf.check_byAvatarUrl.switch === "on") await check_byAvatarUrl();
@@ -36,7 +36,7 @@ async function triggerCheck() {
 }
 
 async function check_byAvatarUrl() {
-    const {wxLogger, wxbot} = env;
+    const {wxLogger, wxbot, secret} = env;
     const t_conf2 = secret.mods.keepalive.check_byAvatarUrl;
     wxbot.userSelf().avatar().then(e => e.toBase64().then(console.log)); // Fallback
     const str = (await wxbot.userSelf().avatar()).toBase64();
