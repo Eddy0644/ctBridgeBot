aa0de5ed6349f4e7132d9d7f49e518a4c10b55a7
Eddy0644 <30020507+Eddy0644@users.noreply.github.com>
2024-10-02 11:31:58 +0800

    chore. keepalive->avoidDuplicateWarn

--- src/m_keepalive.js
+++ src/m_keepalive.js
@@ -12,19 +12,26 @@ async function triggerCheck() {
     const endTime = dayjs().startOf('minute').hour(endHour).minute(endMinute);
     const isNowBetween = dayjs().isAfter(startTime) && dayjs().isBefore(endTime);
 
-    // If within range, start checking
-    if (isNowBetween) {
-        if (secret.misc.debug_show_additional_log) wxLogger.debug("Report on keepalive.triggerCheck #1: ", t_state.msgCounter_prev, " ", state.v.wxStat.MsgTotal);
+    if (isNowBetween) {  // If time between range, start checking
+        if (secret.misc.debug_show_additional_log) wxLogger.debug("Report on keepalive.triggerCheck/msgCount: ", t_state.msgCounter_prev, " ", state.v.wxStat.MsgTotal);
 
         if (t_state.msgCounter_prev < state.v.wxStat.MsgTotal) {
             // There are new messages since last timer run, so update idle timer
             t_state.msgCounter_prev = state.v.wxStat.MsgTotal;
             t_state.idle_start_ts = dayjs().unix();
+            if (t_state.state === -1) {
+                // The bot is operational again! Reset state.
+                t_state.state = 0;
+            }
         } else {
             // No new messages since last timer run, so check if idle timer exceeds
             const idle_length = dayjs().unix() - t_state.idle_start_ts;
             const idle_max = t_conf.trigger_v1[0].max_idle_minutes * 60;
             if (t_state.idle_start_ts !== 0 && idle_length > idle_max) {
+                if (t_state.state === -1) {
+                    // User did not solve the last fail check, so let's just skip
+                    return;
+                }
                 // Idle timer exceeds, so trigger keepalive
                 wxLogger.info(`Keepalive triggered: last update of idle timer is ${dayjs(t_state.idle_start_ts).format("HH:mm")}, which exceeds ${t_conf.trigger_v1[0].max_idle_minutes} minutes from now.`);
                 // Check functions should be executed here...
@@ -39,8 +46,12 @@ async function triggerCheck() {
                     t_state.msgCounter_prev = state.v.wxStat.MsgTotal;
                     t_state.idle_start_ts = dayjs().unix();
                 } else if (ok === 0) {
+                    t_state.state = -1;
+                    // avoid the test message affects checks
+                    t_state.msgCounter_prev++;
                     // Notify user
                     wxLogger.warn(`[Keepalive check] failed, no response received.`);
+
                 }
             }
         }
